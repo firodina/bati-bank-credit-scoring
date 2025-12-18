00a41e2ccb26975e4028dd59856c783f0b1e0244
firodina <rowdahassan49@gmail.com>
2025-12-18 22:39:04 +0300

    Model Deployment  pydantic model

--- src/api/pydantic_models.py
+++ src/api/pydantic_models.py
@@ -1,2 +1,31 @@
-from pydantic import BaseModel
-class Input(BaseModel): feature1: float
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class CreditFeatures(BaseModel):
+    CountryCode: int
+    Amount: float
+    Value: float
+    PricingStrategy: int
+    CurrencyCode_UGX: int
+
+    # # ---------------------------
+    # # Categorical (RAW)
+    # # ---------------------------
+    # ProviderId: int
+    # ProductId: int
+    # ChannelId: int
+
+    # # ---------------------------
+    # # Allowed leakage column (used in training)
+    # # ---------------------------
+    # FraudResult: int
+
+
+class PredictionResponse(BaseModel):
+    default_probability: float = Field(..., example=0.73)
+    risk_label: str = Field(..., example="high")
+
+
+class HealthResponse(BaseModel):
+    status: str = Field(..., example="ok")
+    model_loaded: bool = Field(..., example=True)
